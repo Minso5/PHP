ab96753bbdd7242f68d4e3d13f50d121f8548f19
Minso5 <109052010+Minso5@users.noreply.github.com>
2022-08-09 12:43:58 +0900

    Add files via upload

---
+++ print_test.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Hello HTML
+    Hello HTML
+    <hr>
+    <script>
+        document.write("Hello Javascript ")
+        document.write("Hello Javascript <hr>")
+    </script>
+    <?php
+        print("Hello PHP ");
+        print("Hello PHP ");
+        echo "Hello PHP";
+        echo "<hr>";
+
+        $sum = 1 + 2;
+        echo "$sum <hr>";
+
+        $age = 23;
+        $blood_type = 'B';
+        $name = "Kim Minso";
+        print"나이 : $age <br>";
+        print"혈액형 : $blood_type <br>";
+        print"이름 : $name <br>";
+    ?>
+</body>
+</html>
