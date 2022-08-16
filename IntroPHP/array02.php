e4bdbbed32efa88361d027f62d5a3f6d9de4d0f9
Minso5 <109052010+Minso5@users.noreply.github.com>
2022-08-16 10:25:50 +0900

    Add files via upload

---
+++ IntroPHP/array02.php
@@ -0,0 +1,7 @@
+<?php
+$muitiType = array(20, 3.14, 'A', "Bob");
+print $muitiType[0] . "<br>";
+print $muitiType[1] . "<br>";
+print $muitiType[2] . "<br>";
+print $muitiType[3] . "<br>";
+?>
---
+++ IntroPHP/for02.php
@@ -0,0 +1,8 @@
+<?php
+$i = 1;
+$fact = 1;
+for($i = 1, $fact = 1; $i <= 5 ; $i++){
+    $fact = $fact * $i;
+}
+print "5! = " . $fact;
+?>
