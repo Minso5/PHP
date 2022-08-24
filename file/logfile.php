e758fc1b459213c8d6be35b15d7549e06832d795
Minso5 <109052010+Minso5@users.noreply.github.com>
2022-08-24 12:17:59 +0900

    Add files via upload

---
+++ file/logfile.php
@@ -0,0 +1,21 @@
+<?php
+$filep = fopen("./logfile.txt", "a");
+if(!$filep) die("Can not open log file");
+
+$time = date("Y-m-d H:i:s", time());
+fputs($filep, $time);
+fputs($filep, "\n");
+
+fclose($filep);
+print "Connect Service";
+
+
+$filep = fopen("./logfile.txt", "r");
+if(!$filep) die("Can not open log file");
+
+while($line = fgets($filep, 1024)) {
+    print $line."<br>";
+}
+
+fclose($filep);
+?>
